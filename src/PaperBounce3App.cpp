976ebc33ae4076ed4503db8e0c49f5a525fa1b0d
Chaim Gingold <cg@levitylab.com>
2016-07-13 17:17:24 -0700

    Drawing detected contours.

--- src/PaperBounce3App.cpp
+++ src/PaperBounce3App.cpp
@@ -10,6 +10,53 @@ using namespace ci::app;
 using namespace std;
 
 
+//const cv::RetrievalModes kContourHierStyle = cv::RETR_TREE ;      // whole enchilada
+const cv::RetrievalModes kContourHierStyle = cv::RETR_CCOMP ;     // is just two level, what we want
+//const cv::RetrievalModes kContourHierStyle = cv::RETR_EXTERNAL ;
+
+const float kResScale = 1.f ;
+const float kContourMinRadius = 3.f   * kResScale ;
+const float kContourMinArea   = 80.f * kResScale ;
+const float kContourDPEpislon = 5.f   * kResScale ;
+
+
+const float kBallDefaultRadius = 8.f ;
+
+const vec2 kCaptureSize = vec2( 640, 480 ) ;
+
+namespace cinder {
+	
+	PolyLine2 fromOcv( vector<cv::Point> pts )
+	{
+		PolyLine2 pl;
+		for( auto p : pts ) pl.push_back(vec2(p.x,p.y));
+		pl.setClosed(true);
+		return pl;
+	}
+
+}
+
+class cBall {
+	
+public:
+//	cBall();
+	
+	vec2 mLoc ;
+	vec2 mVel ;
+	float mRadius ;
+	ColorAf mColor ;
+};
+
+class cContour {
+public:
+	PolyLine2	mPolyLine ;
+	vec2		mCenter ;
+	float		mRadius ;
+	float		mArea ;
+	
+	bool		mIsInside = false ;
+};
+
 class PaperBounce3App : public App {
   public:
 	void setup() override;
@@ -17,19 +64,32 @@ class PaperBounce3App : public App {
 	void update() override;
 	void draw() override;
 
+	void tickBalls() ;
+	
+
 	gl::TextureRef			mTexture;
 	CaptureRef				mCapture;
+	
+	vector<cContour>		mContours;
 
+	std::vector<cBall>		mBalls ;
 };
 
 void PaperBounce3App::setup()
 {
-	mCapture = Capture::create( 640, 480 );
+	mCapture = Capture::create( kCaptureSize.x, kCaptureSize.y );
 	mCapture->start();
 }
 
 void PaperBounce3App::mouseDown( MouseEvent event )
 {
+	cBall ball ;
+	
+	ball.mColor = ColorAf::hex(0xC62D41);
+	ball.mLoc   = event.getPos() ;
+	ball.mRadius = kBallDefaultRadius ;
+	
+	mBalls.push_back( ball ) ;
 }
 
 void PaperBounce3App::update()
@@ -66,9 +126,48 @@ void PaperBounce3App::update()
 		vector<vector<cv::Point> > contours;
 		vector<cv::Vec4i> hierarchy;
 		
-		cv::findContours( thresholded, contours, hierarchy, CV_RETR_TREE, CV_CHAIN_APPROX_SIMPLE, cv::Point(0, 0) );
+		cv::findContours( thresholded, contours, hierarchy, kContourHierStyle, CV_CHAIN_APPROX_SIMPLE, cv::Point(0, 0) );
 		
-//		cv::findContours(<#InputOutputArray image#>, <#OutputArrayOfArrays contours#>, <#OutputArray hierarchy#>, <#int mode#>, <#int method#>) ;
+		// filter and process contours into our format
+		mContours.clear() ;
+		for( int i=0; i<contours.size(); ++i )
+		{
+			const auto &c = contours[i] ;
+			
+			cv::Point2f center ;
+			float		radius ;
+			
+			cv::minEnclosingCircle( c, center, radius ) ;
+			
+			float		area = cv::contourArea(c) ;
+			
+			if ( radius > kContourMinRadius && area > kContourMinArea )
+			{
+				auto addContour = [&]( const vector<cv::Point>& c )
+				{
+					cContour myc ;
+					
+					myc.mPolyLine = fromOcv(c) ;
+					myc.mRadius = radius ;
+					myc.mCenter = fromOcv(center) ;
+					myc.mArea   = area ;
+					myc.mIsInside = hierarchy[i][3] != -1 ;
+					
+					mContours.push_back( myc );
+				} ;
+				
+				if (1)
+				{
+					// simplify
+					vector<cv::Point> approx ;
+					
+					cv::approxPolyDP( c, approx, kContourDPEpislon, true ) ;
+					
+					addContour(approx);
+				}
+				else addContour(c) ;
+			}
+		}
 		
 		// convert to texture
 //		mTexture = gl::Texture::create( fromOcv( output ), gl::Texture::Format().loadTopDown() );
@@ -78,14 +177,63 @@ void PaperBounce3App::update()
 
 void PaperBounce3App::draw()
 {
-	if( ( ! mTexture ) ) return;
-	
 	gl::clear();
 	gl::enableAlphaBlending();
+	gl::enable( GL_LINE_SMOOTH );
+	gl::enable( GL_POLYGON_SMOOTH );
+//	gl::enable( gl_point_smooth );
+	glHint( GL_LINE_SMOOTH_HINT, GL_NICEST );
+	glHint( GL_POLYGON_SMOOTH_HINT, GL_NICEST );
+//	glHint( GL_POINT_SMOOTH_HINT, GL_NICEST );
 	
-	gl::setMatricesWindow( getWindowSize() );
-	gl::color( 1, 1, 1 );
-	gl::draw( mTexture );
+	// set window transform
+	{
+		gl::setMatricesWindow( kCaptureSize.x, kCaptureSize.y ); // just at origin; doesn't work for scaled windows
+		
+		// scale up window
+//		float width = (kCaptureSize.y / getWindowSize().y) * (kCaptureSize.x) ;
+//		float centerx = getWindowSize().x / 2.f ;
+//		float left = centerx - width/2 ;
+//		float right = left + width ;
+//
+//		
+//		gl::setMatricesWindow( width, kCaptureSize.y ); // just at origin; doesn't work for scaled windows
+
+		//		left = 0.f ;
+//		right =
+//		gl::setMatrices( CameraOrtho( 0, getWindowSize().x, getWindowSize().y, 0.f, 0.f, 1.f ) ) ;
+			// <- this, but don't deform it.
+	}
+
+	// camera image
+	if ( mTexture )
+	{
+		gl::color( 1, 1, 1 );
+		gl::draw( mTexture );
+	}
+	
+	// draw contours
+	{
+		for( auto c : mContours )
+		{
+			ColorAf color ;
+			
+			if ( c.mIsInside ) color = ColorAf::hex( 0x43730F ) ;
+			else color = ColorAf::hex( 0xF19878 ) ;
+			
+			gl::color(color) ;
+			gl::draw(c.mPolyLine) ;
+		}
+	}
+	
+	// draw balls
+	{
+		for( auto b : mBalls )
+		{
+			gl::color(b.mColor) ;
+			gl::drawSolidCircle( b.mLoc, b.mRadius ) ;
+		}
+	}
 }
 
 CINDER_APP( PaperBounce3App, RendererGl )
