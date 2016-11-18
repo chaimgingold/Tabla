69e739e6bd6e2974a0bfdacb8b4a97fd958c4a71
paulate <paula.m.te@gmail.com>
2016-11-17 17:07:43 -0800

    add up to 7 solenoids now. using switch case.

--- robits/Solenoid-Serial/Solenoid-Serial.ino
+++ robits/Solenoid-Serial/Solenoid-Serial.ino
@@ -3,18 +3,19 @@
 void setup() {
   Serial.begin(115200); //Start serial with baud rate 115200
   Serial.println("Serial begin <3"); 
-  
-  pinMode(13, OUTPUT);
-  digitalWrite(13, LOW);
-}
 
+  for (int x = 2; x <= 13; x++) {
+    pinMode(x, OUTPUT);
+    digitalWrite(x, LOW);
+  }
+}
 
 void loop() {} // nothing in loop
 
 void serialEvent() { // event listener
  while (Serial.available()){
   
-    // parse serial inputs: input is a C string e.g. "HQ" 
+    // parse serial inputs: input is a C string e.g. "A0Q, A1Q" A is the name of the solenoid (or pin) and 0 1 is low or high.  
     // string must end in Q to indicate end of string.
     char input[INPUT_SIZE + 1]; //make a buffer of length 31
     byte size = Serial.readBytesUntil('Q',input, INPUT_SIZE); // read incoming bytes until Q, put them in the buffer named input
@@ -23,19 +24,46 @@ void serialEvent() { // event listener
     // read each command
     // we don't use & in our input string but we might want to if we have different messages. splits a C string into substrings, based on a separator character
     char* command = strtok(input, "&"); 
-    while (command != 0) { 
-      if (command[0] == 'H') {
+    while (command != 0) {
+
+      if (command[0]) {
+        char pin = command[0];
         ++command; // advance the pointer 
-        digitalWrite(13, HIGH);   // send HIGH to pin 13 - ON.
-       
-      }  
-      if (command[0] == 'L') {
-        ++command; // advance the pointer 
-         
-        digitalWrite(13, LOW);    // send LOW
-      }  
+        int msg = atoi(command);
+        solenoidSignal(pin,msg);
+      }
     command = strtok(0, "&"); //goes to next substring 
     } // end while (command != 0)
   } // end while (Serial.available())
 } // end void serialEvent()
+
+
+
+void solenoidSignal(char pinName,int msg) {
+  int pin;
+  switch (pinName) {
+    case 'A':
+      pin = 13;
+      break;
+    case 'B':
+      pin = 3; 
+      break;
+    case 'C':
+      pin = 4;
+      break;
+    case 'D':
+      pin = 5;
+      break;
+    case 'E':
+      pin = 6;
+      break;
+    case 'F':
+      pin = 7;
+      break;
+    case 'G':
+      pin = 8;
+      break;
+  }
+  digitalWrite(pin,msg);
+}
 
