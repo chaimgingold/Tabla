13577b15fe2a18a1fbae1e33670a26ceb9c74ccf
Chaim Gingold <cg@levitylab.com>
2016-12-21 16:04:30 -0800

    refactoring of ocv.h geom.h into .cpp files

---
+++ src/Utilities/geom.cpp
@@ -0,0 +1,146 @@
+//
+//  geom.h
+//  PaperBounce3
+//
+//  Created by Chaim Gingold on 8/4/16.
+//
+//
+
+#include "geom.h"
+
+using namespace cinder;
+using namespace std;
+
+vec2 closestPointOnLineSeg ( vec2 p, vec2 a, vec2 b )
+{
+	vec2 ap = p - a ;
+	vec2 ab = b - a ;
+	
+	float ab2 = ab.x*ab.x + ab.y*ab.y;
+	float ap_ab = ap.x*ab.x + ap.y*ab.y;
+	float t = ap_ab / ab2;
+	
+	if (t < 0.0f) t = 0.0f;
+	else if (t > 1.0f) t = 1.0f;
+	
+	vec2 x = a + ab * t;
+	return x ;
+}
+
+vec2 closestPointOnPoly( vec2 pt, const PolyLine2& poly, size_t *ai, size_t *bi, float* dist )
+{
+	float best = MAXFLOAT ;
+	vec2 result = pt ;
+	
+	// assume poly is closed
+	for( size_t i=0; i<poly.size(); ++i )
+	{
+		size_t j = (i+1) % poly.size() ;
+		
+		vec2 a = poly.getPoints()[i];
+		vec2 b = poly.getPoints()[j];
+
+		vec2 x = closestPointOnLineSeg(pt, a, b);
+		
+		float dist = glm::distance(pt,x) ; // could eliminate sqrt
+
+		if ( dist < best )
+		{
+			best = dist ;
+			result = x ;
+			if (ai) *ai = i ;
+			if (bi) *bi = j ;
+		}
+	}
+	
+	if (dist) *dist = best ;
+	return result ;
+}
+
+bool rayLineSegIntersection( vec2 rayOrigin, vec2 rayDirection, vec2 point1, vec2 point2, float *rayt )
+{
+	// https://gist.github.com/danieljfarrell/faf7c4cafd683db13cbc
+	
+	vec2 v1 = rayOrigin - point1;
+    vec2 v2 = point2 - point1;
+    vec2 v3 = vec2(-rayDirection[1], rayDirection[0]) ;
+    float t1 = cross(vec3(v2,0), vec3(v1,0)).z / dot(v2, v3);
+    float t2 = dot(v1, v3) / dot(v2, v3);
+	
+    if ( t1 >= 0.0 && t2 >= 0.0 && t2 <= 1.f )
+	{
+		if (rayt) *rayt = t1;
+		return true;
+	}
+	return false;
+}
+
+PolyLine2 getPointsAsPoly( const vec2* v, int n )
+{
+	return PolyLine2( vector<vec2>(v,v+n) );
+}
+
+void setPointsFromPoly( vec2* v, int n, PolyLine2 vv )
+{
+	assert( vv.getPoints().size()==n );
+	for( int i=0; i<n; ++i ) v[i] = vv.getPoints()[i];
+}
+
+void appendQuad( TriMesh& mesh, ColorA color, const vec2 v[4], const vec2 uv[4] )
+{
+	/*  0--1
+	    |  |
+		3--2
+	*/
+	
+	int i = mesh.getNumVertices();
+
+	ColorA colors[4] = {color,color,color,color};
+	
+	mesh.appendPositions(v,4);
+	mesh.appendColors(colors,4);
+	if (uv) mesh.appendTexCoords0(uv,4);
+	
+	mesh.appendTriangle(i+0,i+1,i+3);
+	mesh.appendTriangle(i+3,i+1,i+2);
+}
+
+mat4 getRectMappingAsMatrix( Rectf from, Rectf to )
+{
+	mat4 m;
+
+	m *= glm::translate( vec3(  to.getUpperLeft(), 0.f ) );
+
+	m *= glm::scale( vec3(to.getWidth() / from.getWidth(), to.getHeight() / from.getHeight(), 1.f ) );
+
+	m *= glm::translate( vec3( -from.getUpperLeft(), 0.f ) );
+
+	return m;
+}
+
+bool rayIntersectPoly( const PolyLine2& poly, vec2 rayOrigin, vec2 rayVec, float *rayt )
+{
+	bool hit=false;
+	int n = poly.isClosed() ? poly.size() : poly.size()-1; 
+	
+	for( int i=0; i<n; ++i )
+	{
+		int j = (i+1) % poly.size();
+		
+		float t;
+		
+		bool h = rayLineSegIntersection(rayOrigin, rayVec, poly.getPoints()[i], poly.getPoints()[j], &t );
+		
+		if (h)
+		{
+			if (rayt)
+			{
+				if (hit) *rayt = min( *rayt, t ); // min of all
+				else *rayt = t; // first
+			}
+			hit=true;
+		}
+	}
+	
+	return hit;	
+}
--- src/Utilities/ocv.h
+++ src/Utilities/ocv.h
@@ -9,106 +9,26 @@
 #ifndef ocv_h
 #define ocv_h
 
-//#include "opencv2/core/opengl.hpp"
+#include "ocv.h"
+#include "CinderOpenCV.h"
+
+using namespace std;
 
 namespace cinder {
 	
-	inline void vec2toOCV_4( vec2 in[4], cv::Point2f o[4] )
-	{
-		for ( int i=0; i<4; ++i ) o[i] = toOcv( in[i] );
-	}
+	void vec2toOCV_4( vec2 in[4], cv::Point2f o[4] );
 	
-	inline vector<vec2> fromOcv( vector<cv::Point> pts )
-	{
-		vector<vec2> v;
-		for( auto p : pts ) v.push_back(vec2(p.x,p.y));
-		return v;
-	}
+	vector<vec2> fromOcv( vector<cv::Point> pts );
 
-	inline vector<vec2> fromOcv( vector<cv::Point2f> pts )
-	{
-		vector<vec2> v;
-		for( auto p : pts ) v.push_back(vec2(p.x,p.y));
-		return v;
-	}
+	vector<vec2> fromOcv( vector<cv::Point2f> pts );
 
 	// stuff below doesn't really belong in cinder namespace, but whatever.
-	inline glm::mat3x3 fromOcvMat3x3( const cv::Mat& m )
-	{
-		glm::mat3x3 r;
-		
-		for( int i=0; i<3; ++i )
-		{
-			for( int j=0; j<3; ++j )
-			{
-				r[j][i] = m.at<double>(i,j);
-				// opencv: (row,column)
-				// glm:    (column,row)
-			}
-		}
-		
-		return r;
-	}
+	glm::mat3x3 fromOcvMat3x3( const cv::Mat& m );
 
-	inline mat4 mat3to4 ( mat3 i )
-	{
-		mat4 o;
-		
-		// copy upper left 2x2
-		for( int x=0; x<2; ++x )
-		for( int y=0; y<2; ++y )
-		{
-			o[x][y] = i[x][y];
-		}
-		
-		o[0][3] = i[0][2];
-		o[1][3] = i[1][2];
+	mat4 mat3to4 ( mat3 i );
+	mat3 mat4to3 ( mat4 i );
 
-		o[3][0] = i[2][0];
-		o[3][1] = i[2][1];
-		
-		o[3][3] = i[2][2];
-		
-		return o;
-	}
-
-	inline mat3 mat4to3 ( mat4 i )
-	{
-		mat3 o;
-
-		// copy upper left 2x2
-		for( int x=0; x<2; ++x )
-		for( int y=0; y<2; ++y )
-		{
-			o[x][y] = i[x][y];
-		}
-
-		o[0][2] = i[0][3];
-		o[1][2] = i[1][3];
-
-		o[2][0] = i[3][0];
-		o[2][1] = i[3][1];
-
-		o[2][2] = i[3][3];
-
-		return o;
-	}
-
-
-	inline mat4 getOcvPerspectiveTransform( const vec2 from[4], const vec2 to[4] )
-	{
-		cv::Point2f srcpt[4], dstpt[4];
-		
-		for( int i=0; i<4; ++i )
-		{
-			srcpt[i] = toOcv( from[i] );
-			dstpt[i] = toOcv( to[i] );
-		}
-
-		cv::Mat xform = cv::getPerspectiveTransform( srcpt, dstpt ) ;
-		
-		return mat3to4( fromOcvMat3x3(xform) );
-	}
+	mat4 getOcvPerspectiveTransform( const vec2 from[4], const vec2 to[4] );
 
 	template <class T>
 	bool isMatEqual( cv::Mat a, cv::Mat b )
@@ -128,28 +48,7 @@ namespace cinder {
 		return true;
 	}
 	
-	inline gl::TextureRef matToTexture( cv::UMat mat )
-	{
-		/*if (0)
-		{
-			// http://stackoverflow.com/questions/18086519/is-it-possible-to-bind-a-opencv-gpumat-as-an-opengl-texture
-			
-			cv::ogl::Texture2D texture = cv::ogl::Texture2D(mat,false); // no auto-release, we will own it
-
-			return gl::Texture2d::create(
-				GL_TEXTURE_2D, // texture target (is this right?)
-				texture.texId(), // id
-				texture.cols(), texture.rows(), // size
-				false ); // do dispose it. (cinder owns the texture)
-		}
-		else*/
-		{
-	//		mImageGL = gl::Texture::create( fromOcv(mImageCV), gl::Texture::Format().loadTopDown() );
-			return gl::Texture::create(
-				ImageSourceRef( new ImageSourceCvMat( mat.getMat(cv::ACCESS_READ) ) ),
-				gl::Texture::Format().loadTopDown() );
-		}
-	}
+	gl::TextureRef matToTexture( cv::UMat mat );
 	
 }
 
